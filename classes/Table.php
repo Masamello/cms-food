f72908e591fafc67d42b3c034ce6ec012fabcfaa
Masamello <kojimasa1205@gmail.com>
2025-04-08 12:49:02 -0700

    1

--- classes/Table.php
+++ classes/Table.php
@@ -8,10 +8,43 @@
       $this->db = new Database();
     }
 
-    public function addTable() {}
+    public function addTable() {
+      $sql = "INSERT INTO table_tb (TableNumber,Capacity,Location,Status) 
+              VALUES ('A1',6,'Main_Hall','Available')";
+      
+      if($this->db->conn->query($sql) === TRUE) {
+        echo "New table created successfully!";
+      } else {
+        echo "Error: ". $sql . "<br>" . $this->db->conn->error;
+      }
+      $this->db->conn->close();
+    }
 
-    public function updateTable() {}
+    public function updateTable() {
+      $sql = "UPDATE table_tb 
+      SET TableNumber='A2', 
+          Capacity=4, 
+          Location='Sub_Hall',
+          Status='Reserved'
+          WHERE TableId = 1";
 
-    public function deleteTable() {}
+      if($this->db->conn->query($sql) === TRUE) {
+      echo "Table updated successfully!";
+      } else {
+      echo "Error: ". $sql . "<br>" . $this->db->conn->error;
+      }
+      $this->db->conn->close();
+    }
+
+    public function deleteTable(int $id) {
+      $sql = "DELETE FROM table_tb WHERE TableId=$id";
+
+      if($this->db->conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+      } else {
+        echo "Error deleting record" . $this->db->conn->error;
+      }
+      $this->db->conn->close();
+    }
   }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-  require_once "classes/User.php";
+  require_once "classes/Table.php";
 
-  $user = new User();
-  $user->updateUser();
+  $table = new Table();
+  $table->addTable();
 ?>
