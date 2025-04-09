683cab06ac6a88d560ec20aca4ac8b25c05f7c9d
Masamello <kojimasa1205@gmail.com>
2025-04-09 12:02:27 -0700

    1

--- src/models/Roles.php
+++ src/models/Roles.php
@@ -17,11 +17,34 @@
     /**
      * Function to update a role, it's just necessary to update the name of the rule.
      */
-    public function updateRole($roleId, $roleName) {
+    public function updateRole(
+      int $roleId,
+      string $roleName
+      ) {
       try {
-        
+        $sql = "UPDATE roles_tb
+                SET name = '$roleName',
+                WHERE RoleId = $roleId";
+        if($this->db->query($sql)){
+          if($this->db->affected_rows > 0){
+            return[
+              "success" => true,
+              "message" => "Role updated successfully!"
+            ];
+          }else{
+            return [
+              "success" => false,
+              "message" => "No Role were updated."
+            ];
+          }
+        }        
       } catch(\Exception $e) {
-
+        return [
+          "success" => false,
+          "message" => "Something went wrong while updating roles. Please try again shortly."
+        ];
+      } finally {
+        $this->db->close();
       }
     }
   }
